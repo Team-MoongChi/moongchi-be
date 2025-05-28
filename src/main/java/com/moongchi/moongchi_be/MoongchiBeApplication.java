436c407495a0bb2c06d18e91a83eddc9e1fc74f9
ju2hong <juihong730@gmail.com>
2025-05-28 13:20:22 +0900

    chore: Spring Boot 백엔드 프로젝트 초기 업로드

---
+++ src/main/java/com/moongchi/moongchi_be/MoongchiBeApplication.java
@@ -0,0 +1,13 @@
+package com.moongchi.moongchi_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoongchiBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoongchiBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moongchi/moongchi_be/MoongchiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moongchi.moongchi_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoongchiBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
