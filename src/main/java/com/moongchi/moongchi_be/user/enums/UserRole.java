c85a74d8869221ab5e2bb05fc2625f52de29fea1
gaeun.jang <cscom222@naver.com>
2025-05-28 23:11:58 +0900

    feat: UserRole enum 클래스 추가

---
+++ src/main/java/com/moongchi/moongchi_be/user/enums/UserRole.java
@@ -0,0 +1,14 @@
+package com.moongchi.moongchi_be.user.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
+
+    private String value;
+
+    UserRole(String value) {
+        this.value = value;
+    }
+}
