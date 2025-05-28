824481d5fb3a37ef902e8753316bc3fb1d8fc683
zkaakakag <cscom222@naver.com>
2025-05-28 17:43:30 +0900

    feat: JwtAuthenticationFilter 클래스 추가

---
+++ src/main/java/com/moongchi/moongchi_be/auth/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,10 @@
+package com.moongchi.moongchi_be.auth.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+
+}
