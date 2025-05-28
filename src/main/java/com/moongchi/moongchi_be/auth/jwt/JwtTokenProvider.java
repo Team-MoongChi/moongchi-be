40583e31bae22d0d080f9d077718ec0063618668
zkaakakag <cscom222@naver.com>
2025-05-28 17:43:04 +0900

    feat: JwtTokenProvider 클래스 추가

---
+++ src/main/java/com/moongchi/moongchi_be/auth/jwt/JwtTokenProvider.java
@@ -0,0 +1,10 @@
+package com.moongchi.moongchi_be.auth.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenProvider {
+
+}
