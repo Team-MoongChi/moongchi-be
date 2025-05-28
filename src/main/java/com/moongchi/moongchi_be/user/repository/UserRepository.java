61d76bac51928afe6fc36cc0f553ef28022b8314
gaeun.jang <cscom222@naver.com>
2025-05-28 23:09:38 +0900

    feat: UserRepository 생성 및 이메일/소셜 제공자 기반 조회 메서드 추가

---
+++ src/main/java/com/moongchi/moongchi_be/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.moongchi.moongchi_be.user.repository;
+
+import com.moongchi.moongchi_be.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByProviderAndEmail(String provider, String email);
+    Optional<User> findByEmail(String email);
+}
