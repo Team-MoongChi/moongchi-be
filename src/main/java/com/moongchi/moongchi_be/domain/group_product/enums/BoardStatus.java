33b852569c5b621fcb1333082a1b5c9eb0653c77
gaeun.jang <cscom222@naver.com>
2025-05-29 23:05:26 +0900

    feat: 공동구매 게시글 상태 enum 추가

---
+++ src/main/java/com/moongchi/moongchi_be/domain/group_product/enums/BoardStatus.java
@@ -0,0 +1,14 @@
+package com.moongchi.moongchi_be.domain.group_product.enums;
+
+public enum BoardStatus {
+    OPEN("모집중"),
+    CLOSING_SOON("마감임박"),
+    CLOSED("모집마감"),
+    COMPLETED("공구성공");
+
+    private String value;
+
+    BoardStatus(String value) {
+        this.value = value;
+    }
+}
