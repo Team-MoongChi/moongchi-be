54a5dcc0f80d7cb1e2d8ec9476122d706c4ee1a6
gaeun.jang <cscom222@naver.com>
2025-05-29 23:24:27 +0900

    feat: ProductImageRepository 생성

---
+++ src/main/java/com/moongchi/moongchi_be/domain/group_product/repository/ProductImageRepository.java
@@ -0,0 +1,7 @@
+package com.moongchi.moongchi_be.domain.group_product.repository;
+
+import com.moongchi.moongchi_be.domain.group_product.entity.ProductImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+}
