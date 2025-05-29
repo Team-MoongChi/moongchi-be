34bce9eb5ec44913d856c2ed0657f6025dba878d
gaeun.jang <cscom222@naver.com>
2025-05-29 23:23:59 +0900

    feat: GroupBoardRepository 생성

---
+++ src/main/java/com/moongchi/moongchi_be/domain/group_product/repository/GroupBoardRepository.java
@@ -0,0 +1,7 @@
+package com.moongchi.moongchi_be.domain.group_product.repository;
+
+import com.moongchi.moongchi_be.domain.group_product.entity.GroupBoard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupBoardRepository extends JpaRepository<GroupBoard, Long> {
+}
