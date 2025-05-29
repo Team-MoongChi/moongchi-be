f1e0295479ea8d85dcc66f0009a9f9b85a0a51df
gaeun.jang <cscom222@naver.com>
2025-05-29 23:29:09 +0900

    feat: User 엔티티에 user_id 컬럼명 지정 추가

--- src/main/java/com/moongchi/moongchi_be/domain/user/entity/User.java
+++ src/main/java/com/moongchi/moongchi_be/domain/user/entity/User.java
@@ -19,6 +19,7 @@ import java.time.LocalDateTime;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
     @Column
