9bfb67e1bf009cc2551c147674ee909c73943c16
zkaakakag <cscom222@naver.com>
2025-05-30 17:52:14 +0900

    feat: GroupBoard와 맵핑

--- src/main/java/com/moongchi/moongchi_be/domain/user/entity/User.java
+++ src/main/java/com/moongchi/moongchi_be/domain/user/entity/User.java
@@ -1,14 +1,20 @@
 package com.moongchi.moongchi_be.domain.user.entity;
 
+import com.moongchi.moongchi_be.domain.group_boards.entity.GroupBoard;
 import com.moongchi.moongchi_be.domain.user.enums.Gender;
 import com.moongchi.moongchi_be.domain.user.enums.UserRole;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -68,6 +74,9 @@ public class User {
 
     @Column(name = "manner_participant")
     private double mannerParticipant;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<GroupBoard> groupBoards = new ArrayList<>();
 
     @CreationTimestamp
     @Column(name = "create_at")
