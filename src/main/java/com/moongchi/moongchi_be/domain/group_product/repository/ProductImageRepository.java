0a6df87bfb13939a96321f24c03ef3a989f13308
zkaakakag <cscom222@naver.com>
2025-05-30 17:46:59 +0900

    refactor: 불필요한 productImage 엔티티 및 repository 삭제

--- src/main/java/com/moongchi/moongchi_be/domain/group_product/repository/ProductImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.moongchi.moongchi_be.domain.group_product.repository;
-
-import com.moongchi.moongchi_be.domain.group_product.entity.ProductImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
-}
