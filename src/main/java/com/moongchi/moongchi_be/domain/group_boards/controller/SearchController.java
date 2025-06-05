25d0d2e9d21428ca87747fb11ea04aa1184e103d
zkaakakag <cscom222@naver.com>
2025-06-05 16:45:05 +0900

    feat: 공동구매 게시글 검색 구현

---
+++ src/main/java/com/moongchi/moongchi_be/domain/group_boards/controller/SearchController.java
@@ -0,0 +1,25 @@
+package com.moongchi.moongchi_be.domain.group_boards.controller;
+
+import com.moongchi.moongchi_be.domain.group_boards.dto.GroupBoardDto;
+import com.moongchi.moongchi_be.domain.group_boards.service.SearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/group-boards")
+public class SearchController {
+    private final SearchService searchService;
+
+    @GetMapping("/search")
+    public ResponseEntity<List<GroupBoardDto>> search(@RequestParam String keyword){
+        List<GroupBoardDto> groupBoardDtos = searchService.search(keyword);
+        return ResponseEntity.ok(groupBoardDtos);
+    }
+}
--- src/main/java/com/moongchi/moongchi_be/domain/group_boards/repository/GroupBoardRepository.java
+++ src/main/java/com/moongchi/moongchi_be/domain/group_boards/repository/GroupBoardRepository.java
@@ -4,10 +4,12 @@ import com.moongchi.moongchi_be.domain.group_boards.entity.GroupBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 
 import java.util.List;
 
+@Repository
 public interface GroupBoardRepository extends JpaRepository<GroupBoard, Long> {
     @Query(value = "SELECT * FROM group_boards g WHERE " +
             "(6371 * acos(cos(radians(:userLat)) * cos(radians(g.latitude)) * cos(radians(g.longitude) - radians(:userLng)) + sin(radians(:userLat)) * sin(radians(g.latitude)))) < 0.75",
@@ -18,4 +20,5 @@ public interface GroupBoardRepository extends JpaRepository<GroupBoard, Long> {
 
     @Query("SELECT gb FROM GroupBoard gb WHERE gb.groupProduct.category.id = :categoryId")
     List<GroupBoard> findByCategoryId(@Param("categoryId") Long categoryId);
+    List<GroupBoard> findByTitleContaining(String keyword);
 }
