23bcec411e7fc7d6e2bf3d4664aa7e62cfd996e4
zkaakakag <cscom222@naver.com>
2025-06-05 16:47:26 +0900

    style: Schema name 속성을 description으로 변경

--- src/main/java/com/moongchi/moongchi_be/domain/group_boards/dto/GroupBoardRequestDto.java
+++ src/main/java/com/moongchi/moongchi_be/domain/group_boards/dto/GroupBoardRequestDto.java
@@ -4,19 +4,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Schema(description = "공동구매 request Dto")
 public class GroupBoardRequestDto {
 
-    @Schema(name = "이름")
+    @Schema(description = "이름")
     private String name;
 
     private int totalUsers;
