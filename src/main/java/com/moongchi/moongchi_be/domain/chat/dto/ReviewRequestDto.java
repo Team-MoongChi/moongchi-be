f3ecb92a76af0c7947dafccbb8ad93dcdd7c56e6
ju2hong <152269299+ju2hong@users.noreply.github.com>
2025-06-12 15:53:59 +0900

    feat: 채팅 스웨거및 dto 작성

--- src/main/java/com/moongchi/moongchi_be/domain/chat/dto/ReviewRequestDto.java
+++ src/main/java/com/moongchi/moongchi_be/domain/chat/dto/ReviewRequestDto.java
@@ -1,5 +1,6 @@
 package com.moongchi.moongchi_be.domain.chat.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
@@ -10,14 +11,16 @@ import lombok.Setter;
 import java.util.List;
 
 @Getter @Setter
+@Schema(description = "리뷰 작성 요청")
 public class ReviewRequestDto {
     @NotNull
     @DecimalMin("0.5")
     @DecimalMax("5.0")
+    @Schema(description = "별점(0.5~5.0)")
     private Double star;
-
+    @Schema(description = "키워드 리스트(쉼표 구분)")
     @NotBlank
     private List<String> keywords;
-
+    @Schema(description = "리뷰내용")
     private String review;
 }
