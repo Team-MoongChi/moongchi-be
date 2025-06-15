1107a75a108840c788fc0bf1454f9fa24e3caad3
ju2hong <152269299+ju2hong@users.noreply.github.com>
2025-06-15 21:32:54 +0900

    refactor: 채팅 RequestDTO 구조 변경

--- src/main/java/com/moongchi/moongchi_be/domain/chat/dto/ChatMessageRequestDto.java
+++ src/main/java/com/moongchi/moongchi_be/domain/chat/dto/ChatMessageRequestDto.java
@@ -1,17 +1,19 @@
 package com.moongchi.moongchi_be.domain.chat.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @Builder
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatMessageRequestDto {
+    private Long chatRoomId;
+    private Long participantId;
     @NotBlank(message = "메시지를 입력해주세요.")
     private String message;
+    private String messageType;
+
 }
 
