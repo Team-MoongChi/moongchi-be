72644ade60f9bf3da33b82519f216203f3a77c36
gaeun.jang <cscom222@naver.com>
2025-06-16 22:24:19 +0900

    feat: 환경변수 주입 방식 변경

--- src/main/java/com/moongchi/moongchi_be/domain/user/service/LogoutService.java
+++ src/main/java/com/moongchi/moongchi_be/domain/user/service/LogoutService.java
@@ -5,10 +5,17 @@ import com.moongchi.moongchi_be.domain.user.entity.User;
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 @RequiredArgsConstructor
 public class LogoutService {
+
+    @Value("${kakao.client.id}")
+    private String kakaoClientId;
+
+    @Value("${kakao.redirect.uri}")
+    private String kakaoRedirectUri;
 
     private final UserService userService;
 
@@ -22,9 +29,6 @@ public class LogoutService {
     private String socialLogoutRedirectUrl(String provider){
         switch (provider){
             case "kakao":
-                Dotenv dotenv = Dotenv.load();
-                String kakaoClientId = dotenv.get("KAKAO_CLIENT_ID");
-                String kakaoRedirectUri = dotenv.get("KAKAP_REDIRECT_URI");
                 return "https://kauth.kakao.com/oauth/logout?client_id=" + kakaoClientId + "&logout_redirect_uri="
                         + kakaoRedirectUri;
             case "google":
