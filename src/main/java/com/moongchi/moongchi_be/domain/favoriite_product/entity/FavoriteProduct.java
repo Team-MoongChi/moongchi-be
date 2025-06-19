e8e661d9e27d0bbaef437581433942a156724281
gaeun.jang <cscom222@naver.com>
2025-06-19 17:40:04 +0900

    refactor: 오타수정

--- src/main/java/com/moongchi/moongchi_be/domain/favoriite_product/entity/FavoriteProduct.java
+++ src/main/java/com/moongchi/moongchi_be/domain/favoriite_product/entity/FavoriteProduct.java
@@ -29,7 +29,7 @@ public class FavoriteProduct {
     @Column(name = "product_type")
     private ProductType productType;
 
-    @Column(name = "create_at")
+    @Column(name = "created_at")
     @CreationTimestamp
     private LocalDateTime createAt;
 
