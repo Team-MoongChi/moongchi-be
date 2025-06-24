a059f8ac177edb21b613b1ac7af3c6ef662547d9
ju2hong <152269299+ju2hong@users.noreply.github.com>
2025-06-24 09:30:16 +0900

    refactor: 프론트 배포 경로 test

--- src/main/java/com/moongchi/moongchi_be/common/config/SecurityConfig.java
+++ src/main/java/com/moongchi/moongchi_be/common/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://moongchi-phi.vercel.app", "https://moong-chi.kro.kr"));
+        configuration.setAllowedOrigins(Arrays.asList("https://moongchi-phi.vercel.app", "https://moong-chi.kro.kr","https://moongchi-nqeun0cpm-jiwons-projects-be89921d.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
--- src/main/java/com/moongchi/moongchi_be/common/config/WebSocketConfig.java
+++ src/main/java/com/moongchi/moongchi_be/common/config/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat")
-                .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOrigins("https://moongchi-nqeun0cpm-jiwons-projects-be89921d.vercel.app")
                 .withSockJS();
     }
 
