353299f8b98a8daeba4a325abc9ca377a3026d7f
ju2hong <152269299+ju2hong@users.noreply.github.com>
2025-06-25 10:35:20 +0900

    fix: 401 오류 해결

--- src/main/java/com/moongchi/moongchi_be/common/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/moongchi/moongchi_be/common/auth/jwt/JwtAuthenticationFilter.java
@@ -20,6 +20,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
+        String path = request.getRequestURI();
+        if (path.startsWith("/socket")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String token = jwtTokenProvider.resolveToken(request);
         if (token != null && jwtTokenProvider.validateToken(token)) {
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
