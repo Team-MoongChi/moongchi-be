bc0c63003b56edfc0872c61c3a7ee1514de75035
gaeun.jang <cscom222@naver.com>
2025-06-25 14:45:10 +0900

    refactor: 수정 시 제목 형태 변경

--- src/main/java/com/moongchi/moongchi_be/domain/group_boards/service/GroupBoardService.java
+++ src/main/java/com/moongchi/moongchi_be/domain/group_boards/service/GroupBoardService.java
@@ -112,7 +112,7 @@ public class GroupBoardService {
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
 
         groupProduct.update(dto.getName(), dto.getPrice(), dto.getQuantity(), category, dto.getImages());
-        groupBoard.update(dto.getName(), dto.getContent(), dto.getLocation(), coordinate.getLatitude(), coordinate.getLongitude(), dto.getDeadline(), dto.getTotalUser(), groupProduct);
+        groupBoard.update(dto.getName() + " " + dto.getQuantity() + " " + "공구합니다.", dto.getContent(), dto.getLocation(), coordinate.getLatitude(), coordinate.getLongitude(), dto.getDeadline(), dto.getTotalUser(), groupProduct);
 
         groupBoardRepository.save(groupBoard);
     }
