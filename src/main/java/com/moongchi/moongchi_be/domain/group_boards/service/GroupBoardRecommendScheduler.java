f6a21cbb696405222ab7574b0b64b833b2902b89
gaeun.jang <cscom222@naver.com>
2025-07-01 00:09:05 +0900

    test: 추천 캐시 갱신 1분마다 실행

--- src/main/java/com/moongchi/moongchi_be/domain/group_boards/service/GroupBoardRecommendScheduler.java
+++ src/main/java/com/moongchi/moongchi_be/domain/group_boards/service/GroupBoardRecommendScheduler.java
@@ -1,6 +1,5 @@
 package com.moongchi.moongchi_be.domain.group_boards.service;
 
-import com.moongchi.moongchi_be.domain.group_boards.repository.GroupBoardRepository;
 import com.moongchi.moongchi_be.domain.user.entity.User;
 import com.moongchi.moongchi_be.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,11 +15,12 @@ public class GroupBoardRecommendScheduler {
     private final GroupBoardRecommendService groupBoardRecommendService;
     private final UserRepository userRepository;
 
-    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
-    public void updateRecommend(){
+    //    @Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0 * * * * *")
+    public void updateRecommend() {
         List<User> users = userRepository.findAllByIdGreaterThanEqual200();
 
-        for (User user : users){
+        for (User user : users) {
             groupBoardRecommendService.asyncUpdateRecommendCache(user.getId());
         }
 
