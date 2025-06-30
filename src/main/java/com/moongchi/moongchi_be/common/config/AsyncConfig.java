6119d35f6d420d2c03dff6c73bf6450395aa098e
gaeun.jang <cscom222@naver.com>
2025-07-01 00:30:50 +0900

    feat: AsyncConfig 생성

---
+++ src/main/java/com/moongchi/moongchi_be/common/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.moongchi.moongchi_be.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10); // 병렬로 처리할 스레드 수
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("AsyncExecutor-");
+        executor.initialize();
+        return executor;
+    }
+}
